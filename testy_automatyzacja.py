f87740907ceb50b6be0af582c2c000d59fa2f6c7
mrowki35 <115719291+mrowki35@users.noreply.github.com>
2023-02-25 14:55:44 +0100

    Add files via upload

---
+++ testy_automatyzacja.py
@@ -0,0 +1,35 @@
+from openpyxl import Workbook
+from openpyxl import load_workbook
+from openpyxl.utils import get_column_letter
+from bs4 import BeautifulSoup
+import requests
+
+url = "https://www.notino.pl/jimmy-choo/urban-hero-gold-woda-perfumowana-dla-mezczyzn/"
+
+
+result = requests.get(url)
+doc = BeautifulSoup(result.text, "html.parser")
+
+
+
+
+prices = doc.find_all(id="pd-price")
+parent=prices[0].parent
+span = parent.find_all("span")
+price = span[1].text.replace(",",".")
+volume = span[0].text
+
+
+brands = doc.find_all(id="pdHeader")
+brands_parent = brands[0].parent
+stamp = brands_parent.find_all("span")
+genre = stamp[1].text
+brand = stamp[0].text.replace(genre,"")
+category = stamp[2].text
+    
+
+
+wb = load_workbook("Parfume_prices_database.xlsx")
+ws = wb.active
+ws.append([genre,brand,category,volume, float(price),url])
+wb.save("Parfume_prices_database.xlsx")
